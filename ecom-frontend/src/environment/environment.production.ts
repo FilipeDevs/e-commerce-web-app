952cc5c6738bcc4483b7791a5555deb5770e4560
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-12-17 19:11:50 +0100

    config : add env to frontend

---
+++ ecom-frontend/src/environment/environment.production.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  kinde: {
+    authority: 'https://filipedevs.kinde.com',
+    redirectUrl: 'http://localhost:4200',
+    postLogoutRedirectUri: 'http://localhost:4200',
+    clientId: '55f7bb17d4a1413082c9e499acbcd2e3',
+    audience: 'https://filipedevs.kinde.com/api',
+  },
+  apiUrl: 'http://localhost:8080/api'
+};
